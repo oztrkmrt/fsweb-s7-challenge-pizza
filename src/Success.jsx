c2a1f2390f786d0d7fa3aa59a1c511a354e1d946
oztrkmrt <merttoztrk@gmail.com>
2024-07-11 13:13:49 +0300

    test commit

---
+++ src/Success.jsx
@@ -0,0 +1,11 @@
+import './Success.css'
+
+export default function Success() {
+    return (
+      <>
+        <div>TEBRİKLER!</div>
+        <div>SİPARİŞİNİZ ALINDI!</div>
+      </>
+    );
+  }
+  
